0136efb5b8c7ca6b247eb86f203656f082dc9c80
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2021-12-31 23:47:17 +0530

    Add files

---
+++ Understanding the input function on python.py
@@ -0,0 +1,18 @@
+print(input("what is your age "))
+
+print("\n")
+
+age_1=input("what is your age \n")
+print("My age is"+ age_1 )
+
+print("\n")
+
+name=input("what is your name \n")
+
+age=input("what is your age \n")
+
+food=input("what do you want to eat \n")
+
+drink=input("what do u want to drink \n")
+
+print("Hello, my mane is "+name+". I am "+age+". I want to eat "+food+" and "+drink)
