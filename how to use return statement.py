b3bb36554335d49329e15e7adaf64ac8f319b895
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-01 00:25:34 +0530

    Add files

---
+++ how to use return statement.py
@@ -0,0 +1,15 @@
+def calculations (a,b,c):
+    print(a+b+c)
+    print(a*b*c)
+    print(a-b+c)
+
+calculations(1,2,3)
+
+print("\n")
+
+def calculations(a,b,c):
+    print(a+b+c)
+    return print(a*b*c)
+    print(a-b+c)
+
+calculations(2,3,4)
