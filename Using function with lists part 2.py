9cb26da4a89ca0f6b5966907c55d84ec6dd9197a
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-01 04:24:26 +0530

    Add file

---
+++ Using function with lists part 2.py
@@ -0,0 +1,10 @@
+food=["hamburger", "pizza", "juice", "frices", "sushi", "pizza", "juice", "frices"]
+prices=[5.64,3.92,7.29,4.95,3.28]
+
+print(food.count("sushi"))
+
+
+random=food.copy()+prices.copy()
+print(random)
+
+
