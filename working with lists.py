0db63426437c49defe948a8a5e7295e73c36edef
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-01 03:56:13 +0530

    Add file

---
+++ working with lists.py
@@ -0,0 +1,45 @@
+list=["apple", "banana", "grapes", "lemon"]
+
+print (list)
+
+print("\n")
+
+print(list[0])
+
+print("\n")
+
+print(list[1])
+
+print("\n")
+
+print(list[2])
+
+print("\n")
+
+print(list[3])
+
+
+print("\n")
+
+list_1=["apple", "banana", "grapes", "lemon", "strawberry", "other"] 
+
+print("\n")
+
+print(list_1[1:3])
+
+print("\n")
+
+print(list_1[1:4])
+
+
+print("\n")
+
+print(list_1[1:])
+
+print("\n")
+
+print(list_1[2:])
+
+print("\n")
+
+print(list_1[3:])
