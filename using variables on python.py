1f2464622aa2c60842f48e375e66eeb22583ed1d
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-01 16:10:55 +0530

    Add file

--- using variables on python.py
+++ using variables on python.py
@@ -22,4 +22,3 @@ print("hello,"+ person_name)
 print("I am very intrested by your"+object_1+"is it"+ object_1_colour)
 print("also"+person_name+"is your"+object_2+"older than"+ object_2_age)
 print("thank you "+person_name)
-
