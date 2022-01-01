dea733b4d54c9875303949e04307263af828a0c4
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-01 13:14:50 +0530

    Add files

---
+++ Tuples and Lists .py
@@ -0,0 +1,25 @@
+
+
+#tuples
+colour=("red","yellow","blue")
+
+#tuples
+shapes=("square","triangle","circle")
+
+list= colour + shapes
+
+print(list)
+
+print("\n")
+
+#lists
+colour=["red","yellow","blue"]
+
+#tuples
+shapes=("square","triangle","circle")
+
+random = colour + shapes
+
+print(list)
+
+#the second code gives error since its both list and tuple
