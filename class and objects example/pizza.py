782bc7a7b68ea23527a5579512a105aee0896b1e
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-01 15:51:47 +0530

    Add file

---
+++ class and objects example/pizza.py
@@ -0,0 +1,5 @@
+class pizza1 :
+    def __init__(self,type,price,size):
+        self.type=type
+        self.price=price
+        self.size=size
---
+++ class and objects example/project on class and object.py
@@ -0,0 +1,10 @@
+from pizza import pizza1
+pizzav=pizza1("vegitarian",5,"small")
+pizzac=pizza1("chicken",10,"large")
+
+print(pizzav.type)
+print(pizzav.price)
+print(pizzav.size)
+print(pizzac.type)
+print(pizzac.price)
+print(pizzac.size)
